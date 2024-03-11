8faaea94ffa69717919fd60452aab434471f096f
Ravikiran.kc <146708668+24Ravikirankc@users.noreply.github.com>
2024-03-11 16:49:21 +0530

    Create Sample1_App.js
    Practicing

---
+++ Sample1_App.js
@@ -0,0 +1,43 @@
+function App(){
+  const title = 'Blog Spot'
+  const body = 'This is my blog post'
+ const comments = [
+  {id:1, text:'Comment one'},
+  {id:2, text:'Comment two'},
+  {id:3, text:'Comment three'}
+ ]
+ const loading = false
+ 
+ if(loading) return<h1>Loading..</h1>
+  
+  return (
+  <div className="container">
+    <h2>{title.toUpperCase()}</h2>
+  <p>{body}</p>
+
+
+ <div className="comments">
+  <h3> Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+    ))}
+  </ul>
+ </div>
+  </div>
+  )
+}
+export default App
+
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+Out Put
+
+BLOG SPOT
+This is my blog post
+
+
+Comments (3)
+Comment one
+Comment two
+Comment three
+
